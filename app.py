730da466c81bf17a97ac342ab310cec613c10513
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-17 09:35:35 +0530

    Enhance study hours input and prediction process
    Refactor input handling and prediction logic for clarity.

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 import joblib
+import pandas as pd
 
 model = joblib.load("log_reg_model.pkl")
 
-st.write("Number of features:", model.n_features_in_)
-st.write("Feature names:", getattr(model, "feature_names_in_", "Not available"))
 st.title("Student Pass / Fail Based on Study Hours")
-hours = st.number_input("Enter the Study Hours", min_value = 0.0, max_value = 15.0, value = 5.0)
+
+hours = st.number_input(
+    "Enter the Study Hours",
+    min_value=0.0,
+    max_value=15.0,
+    value=5.0,
+    step=0.5
+)
 
 if st.button("Predict"):
-    
-    prediction = model.predict([[hours]])
+
+    input_data = pd.DataFrame({
+        "StudyHours": [hours]
+    })
+
+    prediction = model.predict(input_data)
 
     if prediction[0] == 1:
         st.success("🎉 Student is likely to PASS!")
