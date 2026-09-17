d68ae8bc680e33e9de6e7546581628057af85f5c
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-17 11:13:21 +0530

    Refactor student pass/fail prediction app
    Updated model loading and modified input prompts. Added attendance input for prediction.

--- app.py
+++ app.py
@@ -2,33 +2,40 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-model = joblib.load("log_reg_model.pkl")
+\model = joblib.load("log_reg_hours_model.pkl")
 
-st.title("Student Pass / Fail Based on Study Hours")
+st.title("Student Pass / Fail Prediction")
 
 hours = st.number_input(
-    "Enter the Study Hours",
+    "Enter Study Hours",
     min_value=0.0,
     max_value=15.0,
-    value=5.0,
-    step=0.5
+    value=5.0
+)
+
+attendance = st.number_input(
+    "Enter Attendance (%)",
+    min_value=0.0,
+    max_value=100.0,
+    value=75.0
 )
 
 if st.button("Predict"):
 
     input_data = pd.DataFrame({
-        "StudyHours": [hours]
+        "StudyHours": [hours],
+        "Attendance": [attendance]
     })
 
     prediction = model.predict(input_data)
 
     probabilities = model.predict_proba(input_data)
 
-    fail_probability = probabilities[0][0]
     pass_probability = probabilities[0][1]
 
     if prediction[0] == 1:
         st.success("🎉 Student is likely to PASS!")
     else:
         st.error("❌ Student is likely to FAIL!")
-    st.info(f"Pass Probability: {pass_probability * 100:.2f}%")
+
+    st.write(f"✅ Pass Probability: {pass_probability * 100:.2f}%")
